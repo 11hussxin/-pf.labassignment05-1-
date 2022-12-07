313b9d62402f5f60f4d187c10240fbec963fafab
Hussain Baramatiwala <119995790+11hussxin@users.noreply.github.com>
2022-12-07 04:39:45 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@pflabassignment05-1/main.c
@@ -0,0 +1,85 @@
+
+int main()
+{
+      int ch,arr[3][3],arr2[3][3],arr3[3][3];
+     
+      for(int i=0;i<=2;i++)
+      {
+            for(int j=0;j<=2;j++)
+            {
+                 scanf("%d",&arr[i][j]);
+            }
+      }
+ 
+      for(int i=0;i<=2;i++)
+      {
+            for(int j=0;j<=2;j++)
+            {
+                 printf("%d",arr[i][j]);
+            }
+            printf("\n");
+      }
+
+      for(int i=0;i<=2;i++)
+      {
+            for(int j=0;j<=2;j++)
+            {
+                 scanf("%d",&arr2[i][j]);
+            }
+      }
+       
+      for(int i=0;i<=2;i++)
+      {
+            for(int j=0;j<=2;j++)
+            {
+                 printf("%d",arr2[i][j]);
+            }
+            printf("\n");
+      }
+
+      printf("\n 1.Matrix Addition");
+      printf("\n 2.Matrix substraction");
+      printf("\n please select");
+      scanf("%d",&ch);
+      switch(ch)
+      {
+      case 1:
+      for(int i=0;i<=2;i++)
+      {
+            for(int j=0;j<=2;j++)
+            {
+                 arr3[i][j]=arr[i][j]+arr2[i][j];
+            }
+      }
+
+      for(int i=0;i<=2;i++)
+      {
+            for(int j=0;j<=2;j++)
+            {
+                 printf("%d",arr3[i][j]);
+            }
+            printf("\n");
+      }
+      break;
+      
+      case 2:
+      for(int i=0;i<=2;i++)
+      {
+            for(int j=0;j<=2;j++)
+            {
+                 arr3[i][j]=arr[i][j]-arr2[i][j];
+            }
+      }
+
+      for(int i=0;i<=2;i++)
+      {
+            for(int j=0;j<=2;j++)
+            {
+                 printf("%d",arr3[i][j]);
+            }
+            printf("\n");
+      }
+      break;
+      }
+return 0;
+}
